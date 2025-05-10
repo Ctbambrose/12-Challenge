8eb2090ec2c8cf4647eb95e555190716d2bdf5cb
Ctbambrose <ctbambrose@gmail.com>
2025-05-10 17:46:15 -0500

    footer first MVP done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import { Link, useLocation } from 'react-router-dom';
+
+function Footer() {
+
+    return (
+        <footer className="footer">
+            <nav className='social'>
+                <ul>
+                    <li>
+                        <a>
+                            <img className='social-icon' src='src/assets/InBug-White.png' alt='LinkedIn'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a>
+                            <img className='social-icon' src='src/assets/github-mark-white.png' alt="GitHub"/>
+                        </a>
+                        </li>
+                        <li>
+                        <a>
+                            <img className='social-icon' src='src/assets/Stack_Overflow_icon.png' alt="OverStack"/>
+                        </a>
+                    </li>
+                <img id='gitHub'/>
+                <img id='stackOverflow'/>
+                </ul>
+            </nav>
+        </footer>
+    );
+
+}
+
+export default Footer;
