428f7dae1ef1a268e5aa496cdf09142e15a8ff86
Ctbambrose <ctbambrose@gmail.com>
2025-05-10 21:27:19 -0500

    AboutMe first MVP done

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,6 +1,28 @@
 
-function AboutMe(){
-    
+function AboutMe() {
+    return (
+        <div className="about-me">
+            <article className="picture">
+                <img src='src/assets/Colton-Ambrose.jpeg' alt='Image of Colton Ambrose' />
+            </article>
+            <article className="bio">
+                <h2>
+                    About Me
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
+                    <br />
+                    <br />
+                    Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien
+                    vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus
+                    leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus
+                    bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere.
+                    Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia
+                    nostra inceptos himenaeos.
+                </p>
+            </article>
+        </div>
+    );
 };
 
 export default AboutMe;
