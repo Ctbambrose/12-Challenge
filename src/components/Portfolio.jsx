ceebd7f4f94ced98a38c38f33bc8360c3f43f133
Ctbambrose <ctbambrose@gmail.com>
2025-05-14 16:51:21 -0500

    portfolio first MVP done

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,28 @@
 
 function Portfolio() {
-    return(
+    return (
         <div className="portfolio">
-            <h1>Portfolio</h1>
+            <h2>Portfolio</h2>
+            <div className="projects-grid">
+                <div className="project weather">
+                    <a href="https://github.com/Ctbambrose/09-Challenge"><h3>Weather App</h3></a>
+                </div>
+                <div className="project employee">
+                    <a href="https://github.com/Ctbambrose/10-Challenge"><h3>Employee Tracker</h3></a>
+                </div>
+                <div className="project reunion">
+                    <a href="https://github.com/MattB-ks/01-Project-Family-Reunion"><h3>Family Reunion</h3></a>
+                </div>
+                <div className="project farm">
+                    <a href="https://github.com/Doom825/Farm-Manager"><h3>Farm Managment</h3></a>
+                </div>
+                <div className="project blog">
+                    <a href="https://github.com/Ctbambrose/04-Challenge"><h3>Blog Site</h3></a>
+                </div>
+                <div className="project readme">
+                    <a href="https://github.com/Ctbambrose/07-Challenge"><h3>README Generator</h3></a>
+                </div>
+            </div>
         </div>
     );
 };
